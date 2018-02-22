0eb8e4774455228e6f9053728f7ffc4e9221b745
Mike Treeves <mtreeves24@gmail.com>
2018-02-22 16:34:33 +0000

    add audio on timeout

--- countdown.js
+++ countdown.js
@@ -2,15 +2,14 @@ var body = document.querySelector("body")
 var start = document.querySelector(".startButton")
 var reset = document.querySelector(".resetButton")
 var displayText = document.querySelector("h3")
-var displaySeconds = document.querySelector("span")
 var countDownValue = document.querySelector("input")
-
-currentValue = 60;
+var audio = new Audio("templebell.mp3");
+var currentValue = 60;
 
 
 function countDown(seconds){
-      intervalId = setInterval(function(){
-        seconds --;
+      const intervalId = setInterval(() => {
+        seconds--;
         start.disabled = true;
         if(seconds>10){
             displayText.innerHTML = ("You have " + seconds + " seconds remaining")
@@ -18,38 +17,29 @@ function countDown(seconds){
             displayText.innerHTML = ("You only have " + seconds + " seconds left!")
         } else{
             displayText.innerHTML = ("Time's up!")
-            clearInterval(intervalId)
+            audio.play();
+            clearInterval(intervalId);
             body.classList.toggle("timeup")
         }
     },1000)
 }
 
 
-countDownValue.addEventListener("change", function(){
-    currentValue = this.value;
-    body.classList.remove("timeup")
+countDownValue.addEventListener("change",() => {
+    currentValue = countDownValue.value;
+    body.classList.remove("timeup");
 });
 
-start.addEventListener("click",function(){
-    countDown(currentValue)
+start.addEventListener("click",() => {
+    countDown(currentValue);
+    audio.currentTime = 0;
 });
 
 
-reset.addEventListener("click", function(){
+reset.addEventListener("click", () => {
     displayText.textContent = "Play again?"
     body.classList.remove("timeup")
-    clearInterval(intervalId)
+    audio.pause();
+    // clearInterval(intervalId)
     start.disabled = false;
 })
-
-// function resetInterval() {
-//   clearInterval(intervalId);
-//  }
-
-
-    // reset.addEventListener("click", function(){
-    //     displayText.textContent = "Play again?"
-    //     body.classList.remove("timeup")
-    //     clearInterval(intervalId)
-    // })
-
